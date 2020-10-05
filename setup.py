b45aea337bee3fea48ec6368fd1ca93ed527a100
ABREU ARNAUD <aabreu@unistra.fr>
2020-10-05 16:06:15 +0200

    setup scripts initialized

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup, find_packages
+
+setup(name='pathaia',
+      version='0.1',
+      description='procedures for wsi analysis',
+      author='Arnaud Abreu',
+      author_email='arnaud.abreu.p@gmail.com',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=[],
+      include_package_data=True)
