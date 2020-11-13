45bb84849506f6b2a343be58927788a05f516a53
ABREU ARNAUD <aabreu@unistra.fr>
2020-11-13 15:22:52 +0100

    formated docstrings in patch filters

--- pathaia/patches/filters.py
+++ pathaia/patches/filters.py
@@ -48,12 +48,12 @@ def filter_hasdapi(image, dapi_channel=0, tolerance=1):
     """
     Give presence of dapi in a patch.
 
-    Arguments:
-        - image: image of a patch.
-        - dapi_channel: int, channel to extract dapi signal.
-        - tolerance: int, value on dapi intensity encountered to accept a patch.
+    Args:
+        image (ndarray): image of a patch.
+        dapi_channel (int): channel to extract dapi signal.
+        tolerance (int): value on dapi intensity encountered to accept a patch.
     Returns:
-        - has_dapi: bool, whether dapi is visible in slide.
+        bool: whether dapi is visible in slide.
 
     """
     return (image[:, :, dapi_channel] > tolerance).any() > 0
@@ -63,12 +63,12 @@ def filter_has_significant_dapi(image, dapi_channel=0, tolerance=0.5, dapi_toler
     """
     Give if > 50% of dapi in a patch.
 
-    Arguments:
-        - image: image of a patch.
-        - dapi_channel: int, channel to extract dapi signal.
-        - tolerance: int, value on dapi intensity encountered to accept a patch.
+    Args:
+        image (ndarray): image of a patch.
+        dapi_channel (int): channel to extract dapi signal.
+        tolerance (int): value on dapi intensity encountered to accept a patch.
     Returns:
-        - has_dapi: bool, whether dapi is visible in slide.
+        bool: whether dapi is visible in slide.
 
     """
     return (image[:, :, dapi_channel] > dapi_tolerance).sum() > tolerance * (image.shape[0] * image.shape[1])
@@ -80,13 +80,13 @@ def get_tissue_from_rgb(image, blacktol=0, whitetol=230):
 
     True pixels for the tissue, false pixels for the background.
 
-    Arguments:
-        - image: numpy ndarray, rgb image.
-        - blacktol: float or int, tolerance value for black pixels.
-        - whitetol: float or int, tolerance value for white pixels.
+    Args:
+        image (ndarray): image of a patch.
+        blacktol (float or int): tolerance value for black pixels.
+        whitetol (float or int): tolerance value for white pixels.
 
     Returns:
-        - binarymask: true pixels are tissue, false are background.
+        2D-array: true pixels are tissue, false are background.
 
     """
     binarymask = numpy.zeros_like(image[..., 0], bool)
@@ -105,13 +105,13 @@ def get_tissue_from_lab(image, blacktol=5, whitetol=90):
     This version operates in the lab space, conversion of the image from
     rgb to lab is performed first.
 
-    Arguments:
-        - image: numpy ndarray, rgb image.
-        - blacktol: float or int, tolerance value for black pixels.
-        - whitetol: float or int, tolerance value for white pixels.
+    Args:
+        image (ndarray): image of a patch.
+        blacktol (float or int): tolerance value for black pixels.
+        whitetol (float or int): tolerance value for white pixels.
 
     Returns:
-        - binarymask: true pixels are tissue, false are background.
+        2D-array: true pixels are tissue, false are background.
 
     """
     binarymask = numpy.ones_like(image[..., 0], bool)
@@ -126,14 +126,14 @@ def get_tissue(image, blacktol=5, whitetol=90, method="lab"):
 
     One can choose the segmentation method.
 
-    Arguments:
-        - image: numpy ndarray, rgb image.
-        - blacktol: float or int, tolerance value for black pixels.
-        - whitetol: float or int, tolerance value for white pixels.
-        - method: str, one of 'lab' or 'rgb', function to be called.
+    Args:
+        image (ndarray): image of a patch.
+        blacktol (float or int): tolerance value for black pixels.
+        whitetol (float or int): tolerance value for white pixels.
+        method (str): one of 'lab' or 'rgb', function to be called.
 
     Returns:
-        - binarymask: true pixels are tissue, false are background.
+        2D-array: true pixels are tissue, false are background.
 
     """
     if method not in ["lab", "rgb"]:
@@ -151,13 +151,13 @@ def filter_has_tissue_he(image, blacktol=5, whitetol=90):
     This version operates in the lab space, conversion of the image from
     rgb to lab is performed first.
 
-    Arguments:
-        - image: numpy ndarray, rgb image.
-        - blacktol: float or int, tolerance value for black pixels.
-        - whitetol: float or int, tolerance value for white pixels.
+    Args:
+        image (ndarray): image of a patch.
+        blacktol (float or int): tolerance value for black pixels.
+        whitetol (float or int): tolerance value for white pixels.
 
     Returns:
-        - has_tissue: bool, true if tissue is detected.
+        bool: true if tissue is detected.
 
     """
     return get_tissue_from_lab(image, blacktol=blacktol, whitetol=whitetol).any()
