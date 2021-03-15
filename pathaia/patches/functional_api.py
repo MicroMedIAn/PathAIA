5ce16a80cabfb2e003d0a03b41e4276f4565948f
Robin Schwob <robin.schwob@student.ecp.fr>
2021-03-15 11:24:57 +0100

    added slide filters and integrated it to extract_tissue_patch_coords

--- pathaia/patches/functional_api.py
+++ pathaia/patches/functional_api.py
@@ -18,10 +18,11 @@ from .filters import (
     filter_has_tissue_he,
     standardize_filters,
 )
+from .slide_filters import filter_thumbnail
 import os
 import csv
 from skimage.io import imsave
-from skimage.filters import threshold_otsu
+from skimage.transform import resize
 from tqdm import tqdm
 from .errors import UnknownFilterError
 from pathlib import Path
@@ -32,6 +33,8 @@ izi_filters = {
     "has-significant-dapi": filter_has_significant_dapi,
     "has-tissue-he": filter_has_tissue_he,
 }
+
+slide_filters = {"full": filter_thumbnail}
 
 
 def filter_image(image, filters):
@@ -56,6 +59,26 @@ def filter_image(image, filters):
         else:
             raise UnknownFilterError("{} is not a valid filter !!!".format(filt))
     return True
+
+
+def apply_slide_filters(thumb, filters):
+    """
+    Apply all filters to input thumbnail. Performs logical and between output masks.
+
+    Args:
+        thumb (ndarray): thumbnail to compute mask from.
+        filters (list of str or functions): list of filters to apply to thumb. Each
+            filter should output a boolean mask with same dimensions as thumb.
+
+    Returns:
+        ndarray: Boolean mask where tissue pixels are True.
+    """
+    mask = numpy.ones(thumb.shape[:2], dtype=bool)
+    for filt in filters:
+        if isinstance(filt, str):
+            filt = slide_filters[filt]
+        mask = mask & filt(thumb)
+    return mask
 
 
 def slide_rois(
@@ -519,10 +542,13 @@ def extract_tissue_patch_coords(
     level,
     psize,
     interval,
+    thumb_size=512,
     extensions=(".mrxs",),
     recurse=True,
     folders=None,
     erase_tree=None,
+    filters=None,
+    save_thumbs=False,
 ):
     """
     Extracts all patch coordinates that contain tissue at aspecific level from WSI files
@@ -534,17 +560,24 @@ def extract_tissue_patch_coords(
         level (int): pyramid level to consider.
         psize (int): size of the side of the patches (in pixels).
         interval (dictionary): {"x", "y"} interval between 2 neighboring patches.
+        thumb_size (int): size of thumbnail's longest side. Always preserves aspect ratio.
         extensions (list of str): list of file extensions to consider. Defaults to '.mrxs'.
         recurse (bool): whether to look for files recursively.
         folders (list of str): list of subfolders to explore when recurse is True. Defaults to all.
         erase_tree (bool): whether to erase outfolder if it exists. If None, user will be prompted for a choice.
+        filters (list of str or func): list of filters to apply to thumbnail. Should output boolean mask.
+        save_thumbs (bool): save masked thumbnails of extracted zones.
     """
     outfolder = Path(outfolder)
     if outfolder.is_dir():
         erase_tree = safe_rmtree(outfolder, ignore_errors=True, erase_tree=erase_tree)
     outfolder.mkdir(parents=True, exist_ok=True)
+    if save_thumbs:
+        thumbfolder = outfolder / "thumbnails"
+        thumbfolder.mkdir()
     overlap_size = psize - interval
     files = get_files(infolder, extensions=extensions, recurse=recurse, folders=folders)
+    filters = ifnone(filters, [])
 
     for file in files:
         print(file.stem)
@@ -553,17 +586,22 @@ def extract_tissue_patch_coords(
         psize_0 = dsr * psize
         w, h = slide.dimensions
 
-        thumb_w = int((w / dsr - overlap_size) / interval)
-        thumb_h = int((h / dsr - overlap_size) / interval)
-        thumb = slide.get_thumbnail((thumb_w, thumb_h))
-        thumb = numpy.array(thumb.convert("L"))
-        thr = threshold_otsu(thumb[thumb > 0])
+        thumb = numpy.array(slide.get_thumbnail((thumb_size, thumb_size)))
+        mask = apply_slide_filters(thumb, filters)
+        if save_thumbs:
+            imsave(thumbfolder / f"{file.stem}.png", mask[..., None] * thumb)
+        mask_w = int((w / dsr - overlap_size) / interval)
+        mask_h = int((h / dsr - overlap_size) / interval)
+        mask = resize(mask, (mask_h, mask_w))
 
-        outfile = outfolder / (file.stem + ".csv")
+        outfile = (outfolder / file.relative_to(infolder)).with_suffix(".csv")
+        if not outfile.parent.exists():
+            outfile.parent.mkdir(parents=True)
+
         with open(outfile, "w") as f:
             writer = csv.DictWriter(f, ["id", "parent", "level", "x", "y", "dx", "dy"])
             writer.writeheader()
-            for k, (y, x) in enumerate(numpy.argwhere((thumb > 0) & (thumb < thr))):
+            for k, (y, x) in enumerate(numpy.argwhere(mask)):
                 x = x * interval * dsr
                 y = y * interval * dsr
                 writer.writerow(
