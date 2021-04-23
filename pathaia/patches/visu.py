db9a9549221b3661ee9ba032cc4e26d68e7b8318
Robin Schwob <robin.schwob@student.ecp.fr>
2021-04-23 12:13:57 +0200

    Added very basic test for visu

--- pathaia/patches/visu.py
+++ pathaia/patches/visu.py
@@ -4,7 +4,7 @@
 import numpy
 from skimage.morphology import binary_dilation, disk
 import openslide
-from typing import Sequence, Tuple
+from typing import Sequence, Tuple, Optional
 from ..util.types import NDByteImage, Patch, Coord
 
 
@@ -14,7 +14,8 @@ def preview_from_queries(
     min_res: int = 512,
     color: Tuple[int, int, int] = (255, 255, 0),
     thickness: int = 2,
-    cell_size: int = 20
+    cell_size: int = 20,
+    size_0: Optional[Coord] = None,
 ) -> NDByteImage:
     """
     Give thumbnail with patches displayed.
@@ -26,6 +27,7 @@ def preview_from_queries(
         color: rgb color for patch boundaries
         thickness: thickness of patch boundaries
         cell_size: size of a cell representing a patch in the grid
+        psize: size of a patch at level 0
 
     Returns:
         Thumbnail image with patches displayed.
@@ -33,7 +35,8 @@ def preview_from_queries(
     """
     # get thumbnail first
     slide_size = Coord(slide.dimensions)
-    size_0 = Coord(queries[0].size_0)
+    if size_0 is None:
+        size_0 = Coord(queries[0].size_0)
     res = slide_size / size_0 * (thickness + cell_size) + thickness
     thumb_w = max(min_res, res.x)
     thumb_h = max(min_res, res.y)
@@ -42,7 +45,7 @@ def preview_from_queries(
     dsr = slide_size / thumb_size
     image = numpy.array(image)[:, :, 0:3]
     # get grid
-    grid = 255 * numpy.ones(thumb_size, numpy.uint8)
+    grid = 255 * numpy.ones((thumb_size.y, thumb_size.x), numpy.uint8)
     for query in queries:
         # position in queries are absolute
         x, y = query.position / dsr
---
+++ tests/patches/test_visu.py
@@ -0,0 +1,33 @@
+from tests.helpers import FakeSlide
+from pathaia.patches.visu import preview_from_queries
+from pathaia.util.types import Coord
+import numpy as np
+
+
+def test_preview_from_queries():
+    slide = FakeSlide()
+    queries = []
+    min_res = 512
+    cell_size = 20
+    thickness = 2
+    color = (255, 255, 0)
+    size = Coord(256)
+    level = 1
+    size_0 = size * slide.level_downsamples[level]
+    slide_size = Coord(slide.dimensions)
+    res = slide_size / size_0 * (thickness + cell_size) + thickness
+    thumb_w = max(min_res, res.x)
+    thumb_h = max(min_res, res.y)
+    thumb = np.array(slide.get_thumbnail((thumb_w, thumb_h)).convert("RGB"))
+    assert (
+        preview_from_queries(
+            slide,
+            queries,
+            min_res=min_res,
+            color=color,
+            thickness=thickness,
+            cell_size=cell_size,
+            size_0=size_0,
+        )
+        == thumb
+    ).all()
