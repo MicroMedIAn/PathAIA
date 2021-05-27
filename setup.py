78b376139711843f2be8552a1a367c2887e78739
ArnaudAbreu <arnaud.abreu.p@gmail.com>
2021-05-27 10:38:06 +0200

    new release with datasets and all the cleaning by Robin Schwob

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="pathaia",
-    version="0.1.2",
+    version="0.1.3",
     description="procedures for wsi analysis",
     author="Arnaud Abreu",
     author_email="arnaud.abreu.p@gmail.com",
