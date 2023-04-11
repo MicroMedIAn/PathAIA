3e0325b50dd72f805903a0df7ff4eb597e343b0c
Robin Schwob <robin.schwob@student.ecp.fr>
2023-04-11 10:19:03 +0200

    Added dependency version

--- setup.py
+++ setup.py
@@ -1,36 +1,38 @@
-from setuptools import setup, find_packages
 # read the contents of your README file
 from os import path
+
+from setuptools import find_packages, setup
+
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 setup(
     name="pathaia",
-    version="0.2.1",
+    version="0.2.2",
     description="procedures for wsi analysis",
     author="Arnaud Abreu",
     author_email="arnaud.abreu.p@gmail.com",
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
-        "fastcore",
-        "numpy",
-        "tqdm",
-        "openslide-python",
-        "opencv-python",
-        "scikit-image",
-        "matplotlib",
-        "nptyping",
-        "pandas",
+        "fastcore>=1,<2",
+        "numpy>=1,<2",
+        "tqdm>=4,<5",
+        "openslide-python>=3,<4",
+        "opencv-python>=4,<5",
+        "scikit-image>=0.19,<1",
+        "matplotlib>=3,<4",
+        "nptyping>=2,<3",
+        "pandas>=1,<2",
         "dataclasses",
-        "sortedcontainers",
-        "ordered-set",
-		"shapely",
-		"scikit-learn"
+        "sortedcontainers>=2,<3",
+        "ordered-set>=4,<5",
+        "shapely>=1,<2",
+        "scikit-learn>=1,<2",
     ],
     include_package_data=True,
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type="text/markdown",
 )
