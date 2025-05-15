a837d4e895cdf19811cfb3de183017d625db1293
schwobr <nibormania@yahoo.fr>
2025-05-15 17:08:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="pathaia",
-    version="0.2.4",
+    version="0.2.5",
     description="procedures for wsi analysis",
     author="Arnaud Abreu",
     author_email="arnaud.abreu.p@gmail.com",
